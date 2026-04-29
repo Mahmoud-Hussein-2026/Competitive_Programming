4a40aeca766a8b6d000ed9042797cd94c9242f1f
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-04-29 18:01:59 +0300

    Refactor variable names and improve comments
    Updated variable names and added comments for clarity.

--- problem 200B .java
+++ problem 200B .java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 /*
-
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 200B _ Drinks .
+* Logic : Calculating the average percentage of orang juice by summing all  percentages and dividing by num .
+* Site : Codeforces .
 */
 public class Main{
     public static void main(String[]args){
         Scanner in=new Scanner(System.in);
         int num=in.nextInt();
-        double s=0;
+        // read the number of drinks .
+        double sum=0;
+        // for loop to read each percentage and add it to the sum .
         for(int i=0;i<num;i++){
+            // read the percentage of the current drink .
             int m=in.nextInt();
-            s+=m;
+            sum+=m;
         }
-        System.out.println(s/num);
+        System.out.println(sum/num);
       in.close();
     }
 }
