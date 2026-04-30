f6bc309c4798f33ace3d46ea88a11177f310a688
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-04-30 20:11:30 +0300

    Simplify distance calculation in Beautiful Matrix
    Refactor Manhattan distance calculation for clarity.

--- problem 263A .java
+++ problem 263A .java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 /*
-
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 263A _ Beautiful Matrix .
+* Logic : Calculating the Manhattan distance from the position of 1 to the center of the matrix (2,2) .
+* Site : Codeforces .
 */
 public class Main{
     public static void main(String[]args){
@@ -11,17 +16,21 @@ public class Main{
                 arr[a][b]=in.nextInt();
             }
         }
-        int s=0;
-        int c=0;
+        int rowdiff=0;
+        int coldiff=0;
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
+                // check if the current element is the number one .
                 if(arr[i][j]==1){
-                  s=Math.abs(i-2);
-                  c=Math.abs(j-2);
+                  rowdiff=Math.abs(i-2);
+                  coldiff=Math.abs(j-2);
+                    break;
                 }
                 
             }
         }
-     System.out.println(s+c);
+        // total moves = vertical moves + horizontal moves .
+     System.out.println(rowdiff+coldiff);
+        in.close();
     }
 }
