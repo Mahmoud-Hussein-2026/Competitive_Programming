8d7397e8283b8ab3eb1eb8d41a865ea10b9f4c58
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-01 16:03:59 +0300

    Implement check for substring 'hello' in input

---
+++ problem 58A .java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+     Scanner in=new Scanner(System.in);
+     String n=in.next();
+     String g="hello";
+     int s=n.length();
+     int d=0;
+     for(int i=0;i<s;i++){
+         if(n.charAt(i)==g.charAt(d)){
+             d++;
+         }
+         if(d==5){
+             System.out.println("YES");
+           break;
+         }
+         else{
+             System.out.println("NO");
+         }
+         
+}
+}
