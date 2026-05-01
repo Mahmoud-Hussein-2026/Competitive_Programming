ca0708657c038f4c4afd9a0f76be75b634bdb18c
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-01 18:16:44 +0300

    Close scanner after reading input

--- problem 344A Magnets .java
+++ problem 344A Magnets .java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+/*
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 344A _ Magnets .
+* Logic : Initialize 'groups' counter at 1 , Loop through input magnets Compare current magnet with the previous one
+ * if different, increment 'groups' counter.
+* Site : Codeforces .
+*/
 public class Main{
     public static void main(String[]args){
         Scanner in=new Scanner(System.in);
@@ -13,5 +22,6 @@ public class Main{
              }
          }
       System.out.println(counter);
+        in.close();
      }
   }
