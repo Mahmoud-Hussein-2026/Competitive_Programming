b77e2bd1816dfbfdff21996e1d717db90f572315
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-01 16:30:03 +0300

    Enhance comments and documentation in problem 58A
    Added comments and documentation for clarity.

--- problem 58A .java
+++ problem 58A .java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+/*
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 58A _ Chat room .
+* Logic : Searching for "hello" as a subsequence using a counter .
+* Data : 1 / 5 / 2026 .
+* Site : Codeforces .
+*/
 public class Main{
     public static void main(String[]args){
      Scanner in=new Scanner(System.in);
@@ -6,17 +15,21 @@ public class Main{
      String g="hello";
      int s=n.length();
      int d=0;
+        // d is pointer for the "traget string (hello)" . 
      for(int i=0;i<s;i++){
+         // match found for the current needed character .
          if(n.charAt(i)==g.charAt(d)){
              d++;
          }
+         // all 5 characrers of "hello" found in order .
          if(d==5){
              System.out.println("YES");
            return;
          }
      }
-         
+         // if loop finishes without finding all characters .
              System.out.println("NO");
+        in.close();
          
 }
 }
