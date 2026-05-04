16b64ab8d3bf6f8743380fa9b8d66e634d4a4d6f
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-04 20:47:54 +0300

    Refactor variable names and add comments
    Refactored variable names for clarity and added comments explaining the logic.

--- problem 228A_horseshoe .java
+++ problem 228A_horseshoe .java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 import java.util.HashSet;
+/*
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 228A _ Is your horseshoe on the other hoof .
+* Logic : Using a HashSet to store unique horseshoe colors and calculating
+* the difference from 4 to find the minimum number of shoes to buy .
+* Site : Codeforces .
+*/
 public class Main{
     public static void main(String[]args){
         Scanner in=new Scanner(System.in);
      HashSet<Integer> set=new HashSet<>();
-     int s1=in.nextInt();
-     int s2=in.nextInt();
-     int s3=in.nextInt();
-     int s4=in.nextInt();
-     set.add(s1);
-      set.add(s2);
-       set.add(s3);
-        set.add(s4);
-       int result=4-set.size();
-       System.out.println(result);
+     int color1=in.nextInt();
+     int color2=in.nextInt();
+     int color3=in.nextInt();
+     int color4=in.nextInt();
+     set.add(color1);
+      set.add(color2);
+       set.add(color3);
+        set.add(color4);
+       int shoes_to_buy =4-set.size();
+       System.out.println(shoes_to_buy);
+        in.close();
     }
 }
