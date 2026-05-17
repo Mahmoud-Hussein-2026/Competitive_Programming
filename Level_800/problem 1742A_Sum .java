04e5061165ecc713727c6623ebc3b9634767affe
Mahmoud Hussein <uyfvhiyfv4577@gmail.com>
2026-05-17 17:47:42 +0300

    Refactor comments for problem 1742A - Sum
    Updated problem description and logic explanation in comments.

---
+++ Level_800/problem 1742A_Sum .java
@@ -0,0 +1,28 @@
+/*
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem :  1742A - Sum .
+* Logic : Checks for each testcase if any single integer equals the sum of the ;
+* other two integers using logical OR (||) and equality (==) operators. .
+* Site : Codeforces .
+*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            if(a==b+c||b==a+c||c==a+b){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+      in.close();
+    }
+}
