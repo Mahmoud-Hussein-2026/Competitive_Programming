60480d34cef0ec070cd371e6559d6b27c3f9df36
Mahmoud Hussein <Mahmoud20.dev@gmail.com>
2026-08-21 15:27:22 +0300

    Added solution for problem_2209B_Array.java

--- Level_900/problem_2209B_Array.java
+++ Level_900/problem_2209B_Array.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
-
+/*
+* University : Damascus University .
+* Faculty : Informatics Engineering .
+* Author : Mahmoud Hussein .
+* problem : 2209B - Array .
+* Logic : Count elements strictly greater and smaller than the current element a[i] in the subarray to its right.
+* Site : Codeforces .
+*/
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        if (!sc.hasNextInt()) return;
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            long[] a = new long[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextLong();
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        while (testCases-- > 0) {
+            int arrayLength = in.nextInt();
+            long []numbers = new long[arrayLength];
+            for (int i=0 ; i<arrayLength ; i++) {
+                numbers[i] = in.nextLong();
             }
-            for (int i = 0; i < n; i++) {
+            for (int i=0 ; i<arrayLength ; i++) {
                 int greater = 0;
                 int smaller = 0;
-                for (int j = i + 1; j < n; j++) {
-                    if (a[j] > a[i]) {
+                for (int j = i+1 ; j<arrayLength ; j++) {
+                    if (numbers[j] > numbers[i]) {
                         greater++;
-                    } else if (a[j] < a[i]) {
+                    }
+                    else if (numbers[j] < numbers[i]) {
                         smaller++;
                     }
                 }
-                int ans = Math.max(greater, smaller);
-                System.out.print(ans + (i == n - 1 ? "" : " "));
+                int ans = Math.max( greater , smaller );
+                System.out.print(ans + (i == arrayLength - 1 ? "" : " "));
             }
             System.out.println();
         }
+        in.close();
     }
 }
